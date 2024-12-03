a662ad4e0909da9fb517ad75f10c05a70a25c704
nothing here <roninfortnite71@gmail.com>
2024-12-03 20:14:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/* new RegExp('pattern', 'flags');
+/pattern/f */
+
+/* const ans = prompt('Введите ваше имя');
+
+const reg = /n/ig;
+console.log(reg.test(ans)); */
+
+/* const ans = prompt('Введите ваше число');
+
+const reg = /\d/g;
+console.log(ans.match(reg)); */
+
+const str = 'My name is R2D2';
+
+console.log(str.match(/\D/ig));
+
+// \D
+// \W
+
+// \d
+// \w
+// \s
+// \
+
+// i - 
+// g - 
+// m - 
+
+// console.log(ans.search(reg));
+// console.log(ans.match(reg));
+
+// const pass = prompt('Password');
+
+// console.log(pass.replace(/./g, "*"));
+
+// console.log('12-34-56'.replace(/-/g, ':'));
+
